f02f989f7812074f3024af830fc984c0f4ea02df
jenri019 <j.enrique209436@hotmail.com>
2022-02-22 15:44:11 -0600

    Modifying Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'// Create a Title component that'll render an <h1> tag with some styles
 import styled from 'styled-components'
 
-export const StyledButton = styled.button`
-    background-color: ${(props) =>  props.btnprimary === true ? '#6216c7' : '#5e5e5e'};
+const StyledButton = styled.button`
+    background-color: ${({primary}) =>  primary ? '#6216c7' : '#5e5e5e'};
     border-radius: 5px;
     border-style: none;
     color: #ffffff;
@@ -11,3 +11,11 @@ export const StyledButton = styled.button`
     font-size: 0.8rem;
     font-weight: bold;
 `;
+
+export const Button = ({btnText, primary}) => {
+  return (
+      (primary)
+      ?<StyledButton primary>{btnText}</StyledButton>
+      :<StyledButton>{btnText}</StyledButton>
+  )
+}
